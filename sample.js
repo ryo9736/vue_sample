8e6d2c83c75a7fd16e92b86204c3cf4fd592f821
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-21 19:05:36 +0900

    課題実装

--- sample.js
+++ sample.js
@@ -6,7 +6,7 @@ new Vue({
       name: 'A山B郎',
       cource: 'Webエンジニアコース',
       acceptance_period: '2019年01月期',
-      default_last_id: 3,
+      default_last_id: 4,
       students: [
         { id: 1, name: '野呂浩良', cource: '機械学習コース', acceptance_period: '2019年01月期' },
         { id: 2, name: '富永修司', cource: 'Webエンジニアコース', acceptance_period: '2017年11月期' },
@@ -18,6 +18,11 @@ new Vue({
       addStudent: function() {
           // この function() { } の中にaddStudentメソッド
           // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
+          this.students.push({
+            id: this.default_last_id ++,
+            name: this.name,
+            cource: this.cource,
+            acceptance_period: this.acceptance_period
         })
       }
     }
